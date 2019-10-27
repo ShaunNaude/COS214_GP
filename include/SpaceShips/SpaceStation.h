dffba0348579f80f6c6aba0d7c13e5e707c7149b
Shaun <shaunnaude20@gmail.com>
2019-10-27 13:34:36 +0200

    comments done

--- include/SpaceShips/SpaceStation.h
+++ include/SpaceShips/SpaceStation.h
@@ -4,32 +4,45 @@
 #include <vector>
 #include "ManningTheSpaceships/Commander.h"
 using namespace std;
+
+/**
+ * This class is a signleton in the singleton design pattern.
+ * This class is derived from the spaceships base class.
+ * */
+
 class SpaceStation: public Spaceships
 {
 private:
+    ///This private method has all the possible announcements that the spaceship could broadcast
     string Announcements();
     
 protected:
+    ///Name of the threat level
     string currentThreatLVL;
+    ///Vector of all spaceships visiting spacestations
     vector<Spaceships*> VisitingSpaceships;
+    ///reference to the commander of the spacestation
     Commander* commander;
 
 public:
-    ///functions
-    SpaceStation(/* args */);
+    ///generic Constructor
+    SpaceStation();
+    ///generic Destructor
     ~SpaceStation();
+    ///set method
     void increaseThreatLVL();
+    ///set method
     void decreaseThreatLVL();
+    ///set method
     void setThreatLVL(string);
     string getThreatLVL();
-//-------Radio methods--------------------
+    ///Method to receive message
     void receiveMsg(string);
+    ///method to broadcast announcements
     void makeDailyAnnouncement();
+    ///method to generate report
     void getStatus();
-
-
-//----------------------------------------
-
+    ///method to deplete energy as the spaceships travels
     void DepleteEnergy(int);
     ///Decorator function
     void printComponent();
--- include/SpaceShips/decorator.h
+++ include/SpaceShips/decorator.h
@@ -4,13 +4,21 @@
 #include <iostream>
 using namespace std;
 
+/**
+ * This class is the decorator in the decorator design pattern.
+ * */
+
 class decorator: public Spaceships
 {
 private:
+    ///refrence to the spaceship being decorated 
     Spaceships* decoratedship;
 public:
+    ///fucntion to print 
     virtual void printComponent();
+    ///generic Constructor
     decorator(/* args */);
+    ///generic Destructor
     ~decorator();
 };
 
